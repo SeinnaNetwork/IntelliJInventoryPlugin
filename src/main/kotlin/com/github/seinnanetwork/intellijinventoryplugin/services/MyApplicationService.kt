aa91c3f2145f70f863213f21e93e4c7a860b88b6
GitHub Action <action@github.com>
2022-04-07 19:06:25 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/seinnanetwork/intellijinventoryplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.seinnanetwork.intellijinventoryplugin.services
+
+import com.github.seinnanetwork.intellijinventoryplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/seinnanetwork/intellijinventoryplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/seinnanetwork/intellijinventoryplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.seinnanetwork.intellijinventoryplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.seinnanetwork.intellijinventoryplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
